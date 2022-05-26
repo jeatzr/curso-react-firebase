a16adaa023a0bd4393bfa44d701d6198e6cb9660
jeatzr <profesor.jeatienzar@iesazarquiel.es>
2022-05-26 12:21:45 +0200

    Creamos furmulario para agregar contactos

--- src/App.js
+++ src/App.js
@@ -1,1 +1,32 @@
+import React  from 'react';
+import styled from 'styled-components';
+import Formulario from './componentes/Formulario';
+
+
+const App = () =>
+{
+    return (
+        <Contenedor>
+            <Titulo>Lista de Contactos</Titulo>
+            <Formulario></Formulario>
+        </Contenedor>
+    )
+}
+
+export default App;
+
+const Contenedor = styled.div`
+    margin: 40px;
+    width: 90%;
+    max-width: 400px;
+    background: #fff;    
+    padding: 40px;
+    border-radius: 5p;
+    text-align: center;
+`;
+
+const Titulo = styled.h2`
+    margin-bottom: 10px;
+
+`;
 
